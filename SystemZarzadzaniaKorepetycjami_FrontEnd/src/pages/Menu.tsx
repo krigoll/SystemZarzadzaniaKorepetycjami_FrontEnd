ec9b3533c45b601e5f1f0df2ba5e63a2f5a57c0f
DontPanic1303 <118015465+DontPanic1303@users.noreply.github.com>
2024-08-02 14:17:20 +0200

    jwt decode bug fix

--- SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/Menu.tsx
+++ SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/Menu.tsx
@@ -5,25 +5,44 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../futures/store';
 
 const App: React.FC = () => {
-    const { isAdmin, isTeacher, isStudent } = useSelector((state: RootState) => state.login);
+  const { isAdmin, isTeacher, isStudent } = useSelector(
+    (state: RootState) => state.login
+  );
 
-    return (
-        <div className="App">
-            <div className="sidebar">
-                <AppButton label="Profil" onClick={() => console.log('Profil')} />
-                <AppButton label="Kalendarz" onClick={() => console.log('Kalendarz')} />
-                {isStudent && <AppButton label="Uczeñ" onClick={() => console.log('Uczeñ')} />}
-                {isTeacher && <AppButton label="Nauczyciel" onClick={() => console.log('Nauczyciel')} />}
-                {isAdmin && <AppButton label="Admin" onClick={() => console.log('Nauczyciel')} />}
-                <AppButton label="Wiadomoœci" onClick={() => console.log('Wiadomoœci')} />
-                <AppButton label="Wyloguj siê" onClick={() => console.log('Wyloguj siê')} />
-            </div>
-            <div className="main-content">
-                <button className="cancel-button">Anuluj zajêcia</button>
-                <a href="/help" className="help-link">Masz jakiœ problem? Napisz do nas!</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <AppButton label="Profil" onClick={() => console.log('Profil')} />
+        <AppButton label="Kalendarz" onClick={() => console.log('Kalendarz')} />
+        {isStudent && (
+          <AppButton label="UczeÅ„" onClick={() => console.log('UczeÅ„')} />
+        )}
+        {isTeacher && (
+          <AppButton
+            label="Nauczyciel"
+            onClick={() => console.log('Nauczyciel')}
+          />
+        )}
+        {isAdmin && (
+          <AppButton label="Admin" onClick={() => console.log('Nauczyciel')} />
+        )}
+        <AppButton
+          label="WiadomoÅ›ci"
+          onClick={() => console.log('WiadomoÅ›ci')}
+        />
+        <AppButton
+          label="Wyloguj siÄ™"
+          onClick={() => console.log('Wyloguj siÄ™')}
+        />
+      </div>
+      <div className="main-content">
+        <button className="cancel-button">Anuluj zajÄ™cia</button>
+        <a href="/help" className="help-link">
+          Masz jakiÅ› problem? Napisz do nas!
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default App;
