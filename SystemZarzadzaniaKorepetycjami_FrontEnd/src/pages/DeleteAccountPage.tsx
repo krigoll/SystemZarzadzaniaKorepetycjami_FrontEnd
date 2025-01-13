db5e3bf9a6f07b25163891cd2a168f45f98a5893
krigoll <kristian.gollasch@gmail.com>
2025-01-13 15:07:24 +0100

    profile changes

--- SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/DeleteAccountPage.tsx
+++ SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/DeleteAccountPage.tsx
@@ -8,31 +8,44 @@ import { RootState } from '../futures/store';
 import { useHandleLogOut } from '../lib/LogOut';
 
 const DeleteAccountPage: React.FC = () => {
-  const navigate = useNavigate();
-  const email = useSelector((state: RootState) => state.login.email);
-  const { deletePerson } = usePersonDelete();
-  const handleLogOut = useHandleLogOut();
+    const navigate = useNavigate();
+    const email = useSelector((state: RootState) => state.login.email);
+    const { deletePerson } = usePersonDelete();
+    const handleLogOut = useHandleLogOut();
 
-  const handleDelete = async () => {
-    const status = await deletePerson(email);
+    const handleDelete = async () => {
+        const status = await deletePerson(email);
 
-    if (status === 200) {
-      alert('Person deleted successfully');
-      handleLogOut();
-    } else {
-      alert('Failed to delete person');
-    }
-  };
+        if (status === 200) {
+            alert('Konto zostało usunięte pomyślnie!');
+            handleLogOut();
+        } else {
+            alert('Nie udało się usunąć konta!');
+        }
+    };
 
-  return (
-    <div className="delete-account-page">
-      <h1>Czy aby na pewno chcesz usunąć konto?</h1>
-      <div className="button-container">
-        <AppButton label="Tak" onClick={handleDelete} />
-        <AppButton label="Nie" onClick={() => goToProfile(navigate)} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="delete-account-page">
+            <div className="delete-account-box">
+                <h1>Czy aby na pewno chcesz usunąć konto?</h1>
+                <div className="delete-account-button-container">
+                    <button
+                        className="delete-account-button"
+                        onClick={handleDelete}
+                    >
+                        Tak
+                    </button>
+                    <button
+                        className="delete-account-button no-button"
+                        onClick={() => goToProfile(navigate)}
+                    >
+                        Nie
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default DeleteAccountPage;
--- SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/EditProfile.tsx
+++ SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/EditProfile.tsx
@@ -6,121 +6,121 @@ import { useEditPersonDetails } from '../lib/useEditPersonDetails';
 import { useDispatch } from 'react-redux';
 import Cookies from 'js-cookie';
 import {
-  convertImageToBase64,
-  convertImageToJPEG,
-  isImageFile,
-  resizeImageTo400x400,
+    convertImageToBase64,
+    convertImageToJPEG,
+    isImageFile,
+    resizeImageTo400x400,
 } from '../lib/ConvertImage';
 import {
-  updateEmail,
-  updateStudent,
-  updateTeacher,
+    updateEmail,
+    updateStudent,
+    updateTeacher,
 } from '../futures/login/loginSlice';
 
 const EditProfilePage: React.FC = () => {
-  const location = useLocation();
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const dataToEdit = location.state?.dataToEdit;
-  const navigate = useNavigate();
-  const [profile, setProfile] = useState({
-    idPerson: dataToEdit.idPerson,
-    firstName: dataToEdit.firstName,
-    lastName: dataToEdit.lastName,
-    email: dataToEdit.email,
-    phoneNumber: dataToEdit.phoneNumber,
-    isStudent: dataToEdit.isStudent,
-    isTeacher: dataToEdit.isTeacher,
-    isAdmin: dataToEdit.isAdmin,
-    image: dataToEdit.selectedFile,
-  });
-  const dispatch = useDispatch();
-  const { editPersonDetails, loading, error, responseStatus } =
-    useEditPersonDetails();
-
-  useEffect(() => {
-    if (responseStatus === 200) {
-      dispatch(updateEmail(profile.email));
-      dispatch(updateStudent(profile.isStudent));
-      dispatch(updateTeacher(profile.isTeacher));
-      Cookies.set('email', profile.email, { expires: 1 });
-      goToProfile(navigate);
-    } else if (error) {
-      alert(`Error: ${error}`);
-    }
-  }, [responseStatus, error, profile, dispatch, navigate]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target;
-    setProfile({
-      ...profile,
-      [name]: type === 'checkbox' ? checked : value,
+    const location = useLocation();
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const dataToEdit = location.state?.dataToEdit;
+    const navigate = useNavigate();
+    const [profile, setProfile] = useState({
+        idPerson: dataToEdit.idPerson,
+        firstName: dataToEdit.firstName,
+        lastName: dataToEdit.lastName,
+        email: dataToEdit.email,
+        phoneNumber: dataToEdit.phoneNumber,
+        isStudent: dataToEdit.isStudent,
+        isTeacher: dataToEdit.isTeacher,
+        isAdmin: dataToEdit.isAdmin,
+        image: dataToEdit.selectedFile,
     });
-  };
+    const dispatch = useDispatch();
+    const { editPersonDetails, loading, error, responseStatus } =
+        useEditPersonDetails();
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setSelectedFile(event.target.files[0]);
-    }
-  };
+    useEffect(() => {
+        if (responseStatus === 200) {
+            dispatch(updateEmail(profile.email));
+            dispatch(updateStudent(profile.isStudent));
+            dispatch(updateTeacher(profile.isTeacher));
+            Cookies.set('email', profile.email, { expires: 1 });
+            goToProfile(navigate);
+        } else if (error) {
+            alert(`Błąd: ${error}`);
+        }
+    }, [responseStatus, error, profile, dispatch, navigate]);
 
-  const handleSubmit = async () => {
-    const isValidEmail = (email: string) => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(email);
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value, type, checked } = e.target;
+        setProfile({
+            ...profile,
+            [name]: type === 'checkbox' ? checked : value,
+        });
     };
 
-    const isValidPhoneNumber = (phoneNumber: string) => {
-      return (
-        phoneNumber.length >= 7 &&
-        phoneNumber.length <= 15 &&
-        /^\d+$/.test(phoneNumber)
-      );
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setSelectedFile(event.target.files[0]);
+        }
     };
 
-    if (!isValidEmail(profile.email)) {
-      alert('Nieprawidłowy email.');
-      return;
-    }
+    const handleSubmit = async () => {
+        const isValidEmail = (email: string) => {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+        };
 
-    if (!profile.firstName || !profile.lastName) {
-      alert('Imię i nazwisko nie mogą być puste.');
-      return;
-    }
+        const isValidPhoneNumber = (phoneNumber: string) => {
+            return (
+                phoneNumber.length >= 7 &&
+                phoneNumber.length <= 15 &&
+                /^\d+$/.test(phoneNumber)
+            );
+        };
 
-    if (!isValidPhoneNumber(profile.phoneNumber)) {
-      alert('Nieprawidłowy numer telefonu.');
-      return;
-    }
+        let jpegFile: string | null = null;
+        if (selectedFile != null) {
+            if (!isImageFile(selectedFile)) {
+                alert('Wybrany plik musi być w formacie .png lub .jpg!');
+                return;
+            }
+            let jpgFile = await convertImageToJPEG(selectedFile);
+            jpgFile = await resizeImageTo400x400(jpgFile);
+            jpegFile = await convertImageToBase64(jpgFile);
+        } else if (profile.image != null) {
+            jpegFile = await convertImageToBase64(profile.image);
+        }
 
-    if (!profile.isStudent && !profile.isTeacher && !profile.isAdmin) {
-      alert('Musisz być nauczycielem lub uczniem');
-      return;
-    }
+        if (!isValidEmail(profile.email)) {
+            alert('Nieprawidłowy email!');
+            return;
+        }
 
-    let jpegFile: string | null = null;
-    if (selectedFile != null) {
-      if (!isImageFile(selectedFile)) {
-        alert('Wybrany plik musi być obrazkiem');
-        return;
-      }
-      let jpgFile = await convertImageToJPEG(selectedFile);
-      jpgFile = await resizeImageTo400x400(jpgFile);
-      jpegFile = await convertImageToBase64(jpgFile);
-    } else if (profile.image != null) {
-      jpegFile = await convertImageToBase64(profile.image);
-    }
+        if (!profile.firstName.trim() || !profile.lastName.trim()) {
+            alert('Imię i nazwisko nie mogą być puste!');
+            return;
+        }
 
-    await editPersonDetails({
-      idPerson: profile.idPerson,
-      name: profile.firstName,
-      surname: profile.lastName,
-      email: profile.email,
-      phoneNumber: profile.phoneNumber,
-      image: jpegFile,
-      isStudent: profile.isStudent,
-      isTeacher: profile.isTeacher,
-    });
-  };
+        if (!isValidPhoneNumber(profile.phoneNumber)) {
+            alert('Nieprawidłowy numer telefonu!');
+            return;
+        }
+
+        if (!profile.isStudent && !profile.isTeacher && !profile.isAdmin) {
+            alert('Musisz wybrać rolę!');
+            return;
+        }
+
+        await editPersonDetails({
+            idPerson: profile.idPerson,
+            name: profile.firstName,
+            surname: profile.lastName,
+            email: profile.email,
+            phoneNumber: profile.phoneNumber,
+            image: jpegFile,
+            isStudent: profile.isStudent,
+            isTeacher: profile.isTeacher,
+        });
+    };
 
     return (
         <div className="edit-profile-container">
