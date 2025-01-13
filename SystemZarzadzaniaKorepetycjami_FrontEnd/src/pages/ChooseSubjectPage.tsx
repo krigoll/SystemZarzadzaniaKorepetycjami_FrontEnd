a2e1bf55d3c9c1788b117f15429ee1204ebaaedd
krigoll <kristian.gollasch@gmail.com>
2025-01-13 17:58:18 +0100

    refactor

--- SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/ChooseSubjectPage.tsx
+++ SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/ChooseSubjectPage.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
 import AppButton from '../components/AppButton';
 import { useNavigate } from 'react-router-dom';
-import { goToChooseTeacherPage, goToMenu} from '../lib/Navigate';
+import { goToChooseTeacherPage, goToMenu } from '../lib/Navigate';
 import { useAllSubjects } from '../lib/useAllSubjects';
 
 interface LevelData {
-  level: string;
-  id: number;
+    level: string;
+    id: number;
 }
 
 interface CategoryData {
-  [category: string]: LevelData[];
+    [category: string]: LevelData[];
 }
 
 interface SubjectsData {
-  [subject: string]: CategoryData;
+    [subject: string]: CategoryData;
 }
 
 type Subject = string;
@@ -22,137 +22,137 @@ type Category = string;
 type Level = string;
 
 const ChooseSubjectPage: React.FC = () => {
-  const navigate = useNavigate();
-  const [selectedSubject, setSelectedSubject] = useState<Subject | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
-    null
-  );
-  const [selectedLevel, setSelectedLevel] = useState<Level | null>(null);
-
-  const { subjects, loading, error } = useAllSubjects();
-
-  const processSubjectsData = (
-    data: { subjectFullName: string; subjectLevelId: number }[]
-  ): SubjectsData => {
-    const subjectsData: SubjectsData = {};
-
-    data.forEach(({ subjectFullName, subjectLevelId }) => {
-      const [subject, category, level] = subjectFullName
-        .split(',')
-        .map((part) => part.trim());
-
-      if (!subjectsData[subject]) {
-        subjectsData[subject] = {};
-      }
-
-      if (!subjectsData[subject][category]) {
-        subjectsData[subject][category] = [];
-      }
-
-      if (
-        !subjectsData[subject][category].some((item) => item.level === level)
-      ) {
-        subjectsData[subject][category].push({ level, id: subjectLevelId });
-      }
-    });
-
-    return subjectsData;
-  };
-
-  const subjectsData = processSubjectsData(subjects);
-
-  const handleSubjectClick = (subject: Subject) => {
-    setSelectedSubject(subject);
-    setSelectedCategory(null);
-    setSelectedLevel(null);
-  };
-
-  const handleCategoryClick = (category: Category) => {
-    setSelectedCategory(category);
-    setSelectedLevel(null);
-  };
-
-  const handleLevelClick = (selectedCategory: string, levelData: LevelData) => {
-    setSelectedLevel(levelData.level);
-    goToChooseTeacherPage(
-      navigate,
-      selectedCategory + ' ' + levelData.level + ' ' + levelData.id
+    const navigate = useNavigate();
+    const [selectedSubject, setSelectedSubject] = useState<Subject | null>(null);
+    const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+        null
     );
-  };
+    const [selectedLevel, setSelectedLevel] = useState<Level | null>(null);
 
-  const getCategories = (): string[] => {
-    if (selectedSubject && subjectsData[selectedSubject]) {
-      return Object.keys(subjectsData[selectedSubject]);
-    }
-    return [];
-  };
+    const { subjects, loading, error } = useAllSubjects();
 
-  const getLevels = (): LevelData[] => {
-    if (selectedSubject && selectedCategory && subjectsData[selectedSubject]) {
-      return subjectsData[selectedSubject][selectedCategory] || [];
-    }
-    return [];
-  };
+    const processSubjectsData = (
+        data: { subjectFullName: string; subjectLevelId: number }[]
+    ): SubjectsData => {
+        const subjectsData: SubjectsData = {};
 
-  return (
-      <div className="choose-subject-page">
-          <div className="choose-subject-box">
-              <h1>Wybierz przedmiot, na który chcesz się zapisać.</h1>
-              {loading ? (
-                  <p>Ładowanie przedmiotów...</p>
-              ) : error ? (
-                  <p>Wystąpił błąd: {error}</p>
-              ) : (
-                  <div className="selection-container">
-                      <div className="subject-selection">
-                          <div className="title">Przedmiot</div>
-                          {Object.keys(subjectsData).map((subject) => (
-                              <button
-                                  key={subject}
-                                  className={`option-button ${selectedSubject === subject ? 'selected' : ''}`}
-                                  onClick={() => handleSubjectClick(subject)}
-                              >
-                                  {subject}
-                              </button>
-                          ))}
-                      </div>
-                      {selectedSubject && (
-                          <div className="category-selection">
-                              <div className="title">Kategoria</div>
-                              {getCategories().map((category) => (
-                                  <button
-                                      key={category}
-                                      className={`option-button ${selectedCategory === category ? 'selected' : ''}`}
-                                      onClick={() => handleCategoryClick(category)}
-                                  >
-                                      {category}
-                                  </button>
-                              ))}
-                          </div>
-                      )}
-                      {selectedCategory && (
-                          <div className="level-selection">
-                              <div className="title">Poziom</div>
-                              {getLevels().map((levelData) => (
-                                  <button
-                                      key={levelData.id}
-                                      className={`option-button ${selectedLevel === levelData.level ? 'selected' : ''}`}
-                                      onClick={() => handleLevelClick(selectedCategory, levelData)}
-                                  >
-                                      {selectedCategory} {levelData.level}
-                                  </button>
-                              ))}
-                          </div>
-                      )}
-                  </div>
-              )}
-              <div className="button-container">
-                  <AppButton label="Powrót" onClick={() => goToMenu(navigate)} />
-              </div>
-          </div>
-      </div>
+        data.forEach(({ subjectFullName, subjectLevelId }) => {
+            const [subject, category, level] = subjectFullName
+                .split(',')
+                .map((part) => part.trim());
 
-  );
+            if (!subjectsData[subject]) {
+                subjectsData[subject] = {};
+            }
+
+            if (!subjectsData[subject][category]) {
+                subjectsData[subject][category] = [];
+            }
+
+            if (
+                !subjectsData[subject][category].some((item) => item.level === level)
+            ) {
+                subjectsData[subject][category].push({ level, id: subjectLevelId });
+            }
+        });
+
+        return subjectsData;
+    };
+
+    const subjectsData = processSubjectsData(subjects);
+
+    const handleSubjectClick = (subject: Subject) => {
+        setSelectedSubject(subject);
+        setSelectedCategory(null);
+        setSelectedLevel(null);
+    };
+
+    const handleCategoryClick = (category: Category) => {
+        setSelectedCategory(category);
+        setSelectedLevel(null);
+    };
+
+    const handleLevelClick = (selectedCategory: string, levelData: LevelData) => {
+        setSelectedLevel(levelData.level);
+        goToChooseTeacherPage(
+            navigate,
+            selectedCategory + ' ' + levelData.level + ' ' + levelData.id
+        );
+    };
+
+    const getCategories = (): string[] => {
+        if (selectedSubject && subjectsData[selectedSubject]) {
+            return Object.keys(subjectsData[selectedSubject]);
+        }
+        return [];
+    };
+
+    const getLevels = (): LevelData[] => {
+        if (selectedSubject && selectedCategory && subjectsData[selectedSubject]) {
+            return subjectsData[selectedSubject][selectedCategory] || [];
+        }
+        return [];
+    };
+
+    return (
+        <div className="choose-subject-page">
+            <div className="choose-subject-box">
+                <h1>Wybierz przedmiot, na który chcesz się zapisać.</h1>
+                {loading ? (
+                    <p>Ładowanie przedmiotów...</p>
+                ) : error ? (
+                    <p>Wystąpił błąd: {error}</p>
+                ) : (
+                    <div className="selection-container">
+                        <div className="subject-selection">
+                            <div className="title">Przedmiot</div>
+                            {Object.keys(subjectsData).map((subject) => (
+                                <button
+                                    key={subject}
+                                    className={`option-button ${selectedSubject === subject ? 'selected' : ''}`}
+                                    onClick={() => handleSubjectClick(subject)}
+                                >
+                                    {subject}
+                                </button>
+                            ))}
+                        </div>
+                        {selectedSubject && (
+                            <div className="category-selection">
+                                <div className="title">Kategoria</div>
+                                {getCategories().map((category) => (
+                                    <button
+                                        key={category}
+                                        className={`option-button ${selectedCategory === category ? 'selected' : ''}`}
+                                        onClick={() => handleCategoryClick(category)}
+                                    >
+                                        {category}
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                        {selectedCategory && (
+                            <div className="level-selection">
+                                <div className="title">Poziom</div>
+                                {getLevels().map((levelData) => (
+                                    <button
+                                        key={levelData.id}
+                                        className={`option-button ${selectedLevel === levelData.level ? 'selected' : ''}`}
+                                        onClick={() => handleLevelClick(selectedCategory, levelData)}
+                                    >
+                                        {selectedCategory} {levelData.level}
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                )}
+                <div className="button-container">
+                    <AppButton label="Powrót" onClick={() => goToMenu(navigate)} />
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default ChooseSubjectPage;
--- SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/ReportCreatePage.tsx
+++ SystemZarzadzaniaKorepetycjami_FrontEnd/src/pages/ReportCreatePage.tsx
@@ -6,83 +6,83 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../futures/store';
 
 interface CreateReportProps {
-  IdSender: number;
-  Title: string;
-  Content: string;
-  DateTime: string;
-  IsDealt: boolean;
+    IdSender: number;
+    Title: string;
+    Content: string;
+    DateTime: string;
+    IsDealt: boolean;
 }
 
 const NewReportForm: React.FC = () => {
-  const [title, setTitle] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const navigate = useNavigate();
-  const { createReport, loading, error } = useCreateReport();
-  const uId = useSelector((state: RootState) => state.login.idPerson);
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const navigate = useNavigate();
+    const { createReport, loading, error } = useCreateReport();
+    const uId = useSelector((state: RootState) => state.login.idPerson);
 
-  const handleSubmit = async () => {
-    if (title.length < 1 || title.length > 20) {
-      alert('Tytuł musi mieć pomiędzy 1 a 20 znaków.');
-      return;
-    }
-    if (description.length < 1) {
-      alert('Opis musi mieć pomiędzy 1 a 500 znaków.');
-      return;
-    }
-    const CreateReport: CreateReportProps = {
-      IdSender: uId,
-      Title: title,
-      Content: description,
-      DateTime: new Date().toLocaleString(),
-      IsDealt: false,
+    const handleSubmit = async () => {
+        if (title.trim().length <= 1 || title.trim().length >= 20) {
+            alert('Tytuł musi mieć od 1 do 20 znaków!');
+            return;
+        }
+        if (description.trim().length <= 1 || description.trim().length >= 500) {
+            alert('Opis musi mieć od 1 do 500 znaków!');
+            return;
+        }
+        const CreateReport: CreateReportProps = {
+            IdSender: uId,
+            Title: title,
+            Content: description,
+            DateTime: new Date().toLocaleString(),
+            IsDealt: false,
+        };
+        const returnValue = await createReport(CreateReport);
+        if (returnValue == 0) {
+            alert('Zgłoszenie zostało wysłane pomyślnie!');
+            setTitle('');
+            setDescription('');
+        } else alert('Nie udało się wysłać zgłoszenia!');
     };
-    const returnValue = await createReport(CreateReport);
-    if (returnValue == 0) {
-      alert('Zgłoszenie wysłane pomyśnie!');
-      setTitle('');
-      setDescription('');
-    } else alert('Nie udało się wysłać zgłoszenia!');
-  };
 
-  return (
-    <div className="ticket-form">
-      <div className="ticket-form-box">
-        <h1>Nowe Zgłoszenie</h1>
+    return (
+        <div className="ticket-form">
+            <div className="ticket-form-box">
+                <h1>Nowe Zgłoszenie</h1>
 
-        <div>
-          <div>
-            <label htmlFor="title">Tytuł zgłoszenia</label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Wpisz tytuł zgłoszenia"
-            />
-          </div>
+                <div>
+                    <div>
+                        <label htmlFor="title">Tytuł zgłoszenia</label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder="Wpisz tytuł zgłoszenia"
+                        />
+                    </div>
 
-          <div>
-            <label htmlFor="description">Opis zgłoszenia</label>
-            <textarea
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              placeholder="Wpisz szczegóły zgłoszenia..."
-              rows={6}
-            />
-          </div>
+                    <div>
+                        <label htmlFor="description">Opis zgłoszenia</label>
+                        <textarea
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Wpisz szczegóły zgłoszenia..."
+                            rows={6}
+                        />
+                    </div>
 
-          <div className="ticket-form-actions">
-            <button onClick={() => goToMenu(navigate)}>Powrót</button>
-            <button onClick={handleSubmit} disabled={loading}>
-              Wyślij
-            </button>
-          </div>
-          {error && <p className="error-message">Błąd: {error}</p>}
+                    <div className="ticket-form-actions">
+                        <button onClick={() => goToMenu(navigate)}>Powrót</button>
+                        <button onClick={handleSubmit} disabled={loading}>
+                            Wyślij
+                        </button>
+                    </div>
+                    {error && <p className="error-message">Błąd: {error}</p>}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NewReportForm;
